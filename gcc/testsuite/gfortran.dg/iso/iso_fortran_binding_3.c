e166e4de9d0b8827de11ca6dac9927bd4730577e
Soren Rasmussen <s.c.rasmussen@gmail.com>
2018-11-28 15:57:50 -0800

    18.5.5.2 example in Fortran standard

---
+++ gcc/testsuite/gfortran.dg/iso/iso_fortran_binding_3.c
@@ -0,0 +1,14 @@
+/* From 18.5.5.2 example in the standard */
+/* Test contributed by Soren Rasmussen <S.Rasmussen@cranfield.ac.uk> */
+/* { dg-do compile } */
+#include "ISO_Fortran_binding.h"
+
+void
+test (CFI_cdesc_t *dv)
+{
+  CFI_index_t subscripts[2];
+  float *address;
+  subscripts[0] = 4;
+  subscripts[1] = 9;
+  address = (float *) CFI_address(dv, subscripts);
+}
