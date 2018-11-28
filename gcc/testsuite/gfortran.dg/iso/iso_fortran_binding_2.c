23ee002232136c5001f68c462100d88d75c9e0a6
Soren Rasmussen <s.c.rasmussen@gmail.com>
2018-11-28 15:55:57 -0800

    18.5.4 Note 1 in Fortran standard test

---
+++ gcc/testsuite/gfortran.dg/iso/iso_fortran_binding_2.c
@@ -0,0 +1,12 @@
+/* From 18.5.4 Note 1 in the standard */
+/* Test contributed by Soren Rasmussen <S.Rasmussen@cranfield.ac.uk> */
+/* { dg-do compile } */
+#include "ISO_Fortran_binding.h"
+
+int
+main ()
+{
+  CFI_CDESC_T(5) object;
+  int ind;
+  ind = CFI_deallocate((CFI_cdesc_t *)&object);
+}
