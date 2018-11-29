959cd0a5a78355708a50b0f95c1cb7f734115333
Soren Rasmussen <s.c.rasmussen@gmail.com>
2018-11-28 16:09:47 -0800

    18.5.5.4 example in Fortran standard, CFI_deallocate

---
+++ gcc/testsuite/gfortran.dg/iso/iso_fortran_binding_5.c
@@ -0,0 +1,11 @@
+/* From 18.5.5.4 example in the standard */
+/* Test contributed by Soren Rasmussen <S.Rasmussen@cranfield.ac.uk> */
+/* { dg-do compile } */
+#include "ISO_Fortran_binding.h"
+
+void
+test (CFI_cdesc_t *dv)
+{
+  int ind;
+  ind = CFI_deallocate(dv);
+}
